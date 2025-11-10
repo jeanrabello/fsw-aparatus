8e3ebc4347f67ce4b0d2e4f548b093048cf3495c
Jean Rabello <jean.rabello.prog@gmail.com>
2025-11-10 17:20:24 -0300

    feat: add `booking item` component

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,25 @@ import Image from "next/image";
 import Header from "./_components/header";
 import SearchInput from "./_components/search-input";
 import banner from "../public/banner.png";
+import BookingItem from "./_components/booking-item";
 
 const Home = () => {
   return (
-    <div>
+    <main>
       <Header />
       <div className="px-5 space-y-4">
         <SearchInput />
         <Image src={banner} alt="Agende agora!" sizes="100vw" className="h-auto w-full"/>
+        <h2 className="text-xs text-foreground font-semibold uppercase">Agendamentos</h2>
+        <BookingItem
+          serviceName="Corte de cabelo"
+          barbershopName="Barbearia XYZ"
+          barbershopImageUrl="https://utfs.io/f/5832df58-cfd7-4b3f-b102-42b7e150ced2-16r.png"
+          status="Agendado"
+          date={new Date()}
+        />
       </div>
-    </div>
+    </main>
   );
 }
 
